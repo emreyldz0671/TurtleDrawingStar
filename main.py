eb8010a203c0292e39b91fcdbf3e77458afe93f4
Emre Yıldız <emreeyldz06@gmail.com>
2023-09-05 14:55:22 +0300

    DifferentDrawingStar added

---
+++ main.py
@@ -0,0 +1,48 @@
+import turtle
+
+drawing_board = turtle.Screen()
+drawing_board.bgcolor("darkmagenta")
+drawing_board.title("Python Turtle")
+
+#1st Way is true drawing
+'''turtle_instance = turtle.Turtle()
+for i in range(5):
+    turtle_instance.right(144)
+    turtle_instance.forward(135)
+turtle.done()'''
+
+#2nd Way is my different drawing
+'''turtle_instance.right(135)
+turtle_instance.forward(135)
+turtle_instance.right(150)
+turtle_instance.forward(135)
+turtle_instance.left(90)
+turtle_instance.forward(135)
+turtle_instance.right(165)
+turtle_instance.forward(135)
+turtle_instance.left(75)
+turtle_instance.forward(135)
+turtle_instance.right(150)
+turtle_instance.forward(135)
+turtle_instance.left(75)
+turtle_instance.forward(135)
+turtle_instance.right(155)
+turtle_instance.forward(120)
+turtle_instance.left(90)
+turtle_instance.forward(135)
+turtle_instance.right(160)
+turtle_instance.forward(140)'''
+
+#3rd Way is my anaother different drawing
+'''turtle_instance.left(45)
+turtle_instance.forward(135)
+turtle_instance.left(45)
+turtle_instance.backward(135)
+turtle_instance.left(45)
+turtle_instance.forward(135)
+turtle_instance.left(45)
+turtle_instance.backward(135)
+turtle_instance.left(23)
+turtle_instance.forward(144)'''
+#turtle.done()
+
